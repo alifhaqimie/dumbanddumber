30981a7f58ffcbfe0dc4e636c59109d480fd3a02
alifhaqimie <64553720+alifhaqimie@users.noreply.github.com>
2020-05-16 20:35:17 +0100

    j

--- src/sample/Controller/RegisterController.java
+++ src/sample/Controller/RegisterController.java
@@ -1,14 +1,19 @@
 package sample.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import sample.Animation.Shaker;
 import sample.Database.DatabaseHandler;
 import sample.Model.User;
